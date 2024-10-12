d8f7a1be69c7a9e62992121145a66e2d6fca7c9d
jereck2023 <jereck2020@gmail.com>
2024-10-12 06:38:23 -0500

    app-gateway-pena

---
+++ src/main/java/pe/edu/cibertec/app_gateway_pena/AppGatewayPenaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_gateway_pena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGatewayPenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGatewayPenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_gateway_pena/AppGatewayPenaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_gateway_pena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayPenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
